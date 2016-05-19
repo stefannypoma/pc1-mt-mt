48f7dcf849356a4cf9eae569ff626cc8642b86c1
stefannypoma <stefanny.pomab@gmail.com>
2016-05-18 20:23:37 -0500

    Create Message.java

---
+++ app/src/main/java/pe/edu/ulima/pc_1_mt/agregarAlumno/Message.java
@@ -0,0 +1,19 @@
+public class Message {
+
+    private String msg;
+
+    public Message(String msg) {
+        this.msg = msg;
+    }
+
+    public Message() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
